77956d22fb96e2837f1998330e3a5adf5ebc0be9
Alex Vihman <alexg657@gmail.com>
2020-10-02 14:45:08 +0300

    api key

--- controllers/image.js
+++ controllers/image.js
@@ -1,7 +1,7 @@
 const Clarifai = require('clarifai');
 
 const app = new Clarifai.App({
-    apiKey: 'ed1e6de39f3a46a4b9d131665b07800a'
+    apiKey: process.env.CLARIFAI_KEY
 });
 
 const handleImage = (req, res, knex) => {
