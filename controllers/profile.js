d7ec21814fdff9bf2f6a7ca5e4f4869021afe956
Alex Vihman <alexg657@gmail.com>
2020-10-04 19:16:31 +0300

    add

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,25 +1,25 @@
 const handleProfile = (req, res, knex, bcrypt) => {
 
     const { name, email, password } = req.body;
-    if (name) {
-        knex('users')
-            .where({ email })
-            .returning('*')
-            .update({
-                name: name
-            })
+    // if (name) {
+    //     knex('users')
+    //         .where({ email })
+    //         .returning('*')
+    //         .update({
+    //             name: name
+    //         })
 
-            .then(user => {
-                if (user.length) {
-                    res.json(user[0])
-                }
-                else {
-                    res.status(404).json('user not found')
-                }
+    //         .then(user => {
+    //             if (user.length) {
+    //                 res.json(user[0])
+    //             }
+    //             else {
+    //                 res.status(404).json('user not found')
+    //             }
 
-            })
-            .catch(err => res.status(400).json('unable to update user'))
-    }
+    //         })
+    //         .catch(err => res.status(400).json('unable to update user'))
+    // }
     if (password) {
         const hash = bcrypt.hashSync(password);
         knex('login')
@@ -27,7 +27,7 @@ const handleProfile = (req, res, knex, bcrypt) => {
             .update({
                 hash: hash
             })
-            .then(
+            .then(()=>
                 knex('users')
                     .where({ email })
                     .then(user => {
