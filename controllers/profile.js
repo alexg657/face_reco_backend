5b2c21e75ac20c9b159b23b3fc8051d805a3f745
Alex Vihman <alexg657@gmail.com>
2020-10-04 19:05:05 +0300

    add

--- controllers/profile.js
+++ controllers/profile.js
@@ -1,24 +1,50 @@
 const handleProfile = (req, res, knex, bcrypt) => {
 
     const { name, email, password } = req.body;
+    if (name) {
+        knex('users')
+            .where({ email })
+            .returning('*')
+            .update({
+                name: name
+            })
+            .then(
+                knex('users')
+                    .where({ email })
+                    .then(user => {
+                        if (user.length) {
+                            res.json(user[0])
+                        }
+                        else {
+                            res.status(404).json('user not found')
+                        }
 
-    knex('users')
-        .where({ email })
-        .returning('*')
-        .update({
-            name: 'archived'
-        })
-        
-        .then(user => {
-            if (user.length) {
-                res.json(user[0])
-            }
-            else {
-                res.status(404).json('user not found')
-            }
+                    })
+            )
 
-        })
-        .catch(err => res.status(400).json('unable to update user'))
+            .catch(err => res.status(400).json('unable to update user'))
+    }
+    if (password) {
+        const hash = bcrypt.hashSync(password);
+        knex('login')
+            .where({ email })
+            .returning('*')
+            .update({
+                hash: hash
+            })
+
+            .then(user => {
+                if (user.length) {
+                    res.json(user[0])
+                }
+                else {
+                    res.status(404).json('user not found')
+                }
+
+            })
+            .catch(err => res.status(400).json('unable to update user'))
+    }
+
 
 
 
