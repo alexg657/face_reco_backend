f6853b5cea5205f5ebfc257ea3f5510cd969f2de
Alex Vihman <alexg657@gmail.com>
2020-10-29 18:36:11 +0200

    new function

--- server.js
+++ server.js
@@ -24,15 +24,7 @@ var knex = require('knex')({
 
 app.use(express.json());//middleware
 
-//app.use(cors());
-
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+app.use(cors());
 
 app.post('/signin', (req, res) => {
     handleSignin.handleSignin(req, res, bcrypt, knex)
