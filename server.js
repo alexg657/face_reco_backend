ce018806e01835e6baf5fb8dd5ef01f0f3acb02f
Alex Vihman <alexg657@gmail.com>
2020-10-29 18:51:26 +0200

    new function

--- server.js
+++ server.js
@@ -24,13 +24,8 @@ var knex = require('knex')({
 
 app.use(express.json());//middleware
 
-//app.use(cors());
-app.all('*', function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-    res.header('Access-Control-Allow-Headers', 'Content-Type');
-    next();
-  });
+app.use(cors());
+app.options('*', cors());
 
 
 app.post('/signin', (req, res) => {
