009d3316ecae7b624416fc370922967c53e7bce4
Alex Vihman <alexg657@gmail.com>
2020-10-29 18:35:04 +0200

    new function

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ var knex = require('knex')({
 
 app.use(express.json());//middleware
 
-app.use(cors());
+//app.use(cors());
 
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", '*');
