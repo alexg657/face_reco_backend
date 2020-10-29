105f261a8ac11dfa0e671db1805f63e4c4fb4139
Alex Vihman <alexg657@gmail.com>
2020-10-29 18:31:36 +0200

    new function

--- server.js
+++ server.js
@@ -26,6 +26,13 @@ app.use(express.json());//middleware
 
 app.use(cors());
 
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", '*');
+    res.header("Access-Control-Allow-Credentials", true);
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
+    next();
+});
 
 app.post('/signin', (req, res) => {
     handleSignin.handleSignin(req, res, bcrypt, knex)
